24291d484fbb9f673baf68343a81e9cd293df1b9
Adri <apereg24@estudiantes.unileon.es>
2020-03-10 17:38:26 +0100

    Practical lesson 10/03 with Pablete

--- ed-a002-2020/src/ule/edi/queuewithrep/ArrayQueueWithRepImpl.java
+++ ed-a002-2020/src/ule/edi/queuewithrep/ArrayQueueWithRepImpl.java
@@ -2,156 +2,188 @@ package ule.edi.queuewithrep;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.Random;
 
 import ule.edi.exceptions.EmptyCollectionException;
 
 public class ArrayQueueWithRepImpl<T> implements QueueWithRep<T> {
-	
+
 	// atributos
-	
-    private final int capacityDefault = 10;
-	
+
+	private final int capacityDefault = 10;
+
 	ElemQueueWithRep<T>[] data;
-    private int count;
-    
-	// Clase interna 
-    
+	private int count;
+
+	// Clase interna
+
 	@SuppressWarnings("hiding")
 	public class ElemQueueWithRep<T> {
 		T elem;
 		int num;
-		public ElemQueueWithRep (T elem, int num){
-			this.elem=elem;
-			this.num=num;
+
+		public ElemQueueWithRep(T elem, int num) {
+			this.elem = elem;
+			this.num = num;
 		}
 	}
 
-	
 	///// ITERADOR //////////
 	@SuppressWarnings("hiding")
 	public class ArrayQueueWithRepIterator<T> implements Iterator<T> {
-		
-	
-		public ArrayQueueWithRepIterator(ElemQueueWithRep<T>[] cola, int count){
-					
+
+		int count, current;
+		ElemQueueWithRep<T>[] cola;
+
+		public ArrayQueueWithRepIterator(ElemQueueWithRep<T>[] cola, int count) {
+			this.count = count;
+			this.current = 0;
+			this.cola = cola;
 		}
 
 		@Override
 		public boolean hasNext() {
-			
-		
+			return current < count;
 		}
 
 		@Override
 		public T next() {
-		
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			current++;
+			return cola[current - 1].elem;
 		}
-		
-		
 
 	}
 	////// FIN ITERATOR
-	
-	
-    // Constructores
+
+	// Constructores
 
 	@SuppressWarnings("unchecked")
 	public ArrayQueueWithRepImpl() {
-		data =   new ElemQueueWithRep[capacityDefault];
-		count=0;
+		data = new ElemQueueWithRep[capacityDefault];
+		count = 0;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public ArrayQueueWithRepImpl(int capacity) {
-		data =  new ElemQueueWithRep[capacity];
-		count=0;
+		data = new ElemQueueWithRep[capacity];
+		count = 0;
+	}
+
+	@SuppressWarnings("unchecked")
+	private void expandCapacity() {
+		ElemQueueWithRep<T>[] nuevo = (ElemQueueWithRep<T>[]) new ElemQueueWithRep[data.length * 2];
+		for (int i = 0; i < data.length; i++) {
+			nuevo[i] = data[i];
+		}
+		data = nuevo;
+		count++;
+	}
+
+	@Override
+	public void add(T element, int times) {
+		// TODO
+
+	}
+
+	@Override
+	public void add(T element) {
+		// TODO
+
+	}
+
+	@Override
+	public void remove(T element, int times) {
+		if (this.isEmpty() || !this.contains(element))
+			throw new NoSuchElementException();
+		int pos = this.find(element);
+		if(this.data[i].num > times)
+	}
+
+	@Override
+	public int remove() throws EmptyCollectionException {
+		if (this.isEmpty())
+			throw new EmptyCollectionException("ArrayQueueWithRep");
+		int apariciones = this.data[0].num;
+
+		if (count > 1) {
+			this.data[0] = this.data[count - 1];
+			this.data[count - 1] = null;
+		} else {
+			this.data[0] = null;
+		}
+		count--;
+		return apariciones;
+	}
+
+	@Override
+	public void clear() {
+		for (int i = 0; i < this.count; i++) {
+			this.data[i] = null;
+		}
+	}
+
+	@Override
+	public boolean contains(T element) {
+		for (int i = 0; i < count; i++) {
+			if (element.equals(data[i]))
+				return true;
+		}
+		return false;
+
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return count == 0;
+	}
+
+	@Override
+	public long size() {
+		long num = 0;
+		for (int i = 0; i < this.count; i++)
+			num += this.data[i].num;
+		return num;
+
+	}
+
+	@Override
+	public int count(T element) {
+		if(this.contains(element))
+			return this.data[this.find(element)].num;
+		return 0;
+
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return new ArrayQueueWithRepIterator<T>(this.data, this.count);
+
+	}
+
+	@Override
+	public String toString() {
+
+		final StringBuffer buffer = new StringBuffer();
+
+		buffer.append("(");
+
+		// TODO Ir añadiendo en buffer las cadenas para la representación de la cola.
+		// Ejemplo: (A, A, A, B )
+
+		buffer.append(")");
+
+		return buffer.toString();
 	}
 	
-	
-	 @SuppressWarnings("unchecked")
-	 private void expandCapacity() {
-		
-			ElemQueueWithRep<T>[] nuevo= (ElemQueueWithRep<T>[]) new ElemQueueWithRep[data.length*2];
-			
-			// todo
+	private int find(T element) {
+		for (int i = 0; i < this.count; i++) {
+			if(this.data[i].elem.equals(element))
+				return i;
 		}
-	 
-	
-			@Override
-			public void add(T element, int times) {
-				// TODO 
-			
-			}
-			
+		return -1;		
+	}
 
-			@Override
-			public void add(T element) {
-				// TODO 
-				
-				}
-
-			@Override
-			public void remove(T element, int times) {
-				//todo
-			}
-
-			@Override
-			public int remove() throws EmptyCollectionException {
-				//todo
-				
-			}
-
-			@Override
-			public void clear() {
-				// TODO 
-				
-			}
-			
-
-			@Override
-			public boolean contains(T element) {
-				// TODO 
-			
-
-			}
-
-			@Override
-			public boolean isEmpty() {
-				// TODO 
-				
-			}
-
-			@Override
-			public long size() {
-				// TODO 
-				
-			}
-
-			@Override
-			public int count(T element) {
-				// TODO 
-				
-			}
-
-			@Override
-			public Iterator<T> iterator() {
-				// TODO 
-				
-			}
-			
-			@Override
-			public String toString() {
-				
-				final StringBuffer buffer = new StringBuffer();
-				
-				buffer.append("(");
-
-				// TODO Ir añadiendo en buffer las cadenas para la representación de la cola. Ejemplo: (A, A, A, B )
-				
-				buffer.append(")");
-				
-				return buffer.toString();
-			}
-
-	
 }
